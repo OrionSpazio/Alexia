c5e0b34d280563978bcba5bddd72393619bb0e7b
OrionSpazio <gdrol038@uottawa.ca>
2015-11-02 22:10:27 -0500

    messages

---
+++ Messages.php
@@ -0,0 +1,43 @@
+<html>
+<body>
+<center>
+<textarea cols="100" rows="15" readonly>
+<?php
+$servername = "us-cdbr-azure-northcentral-a.cleardb.com";
+$username = "bcc07699641851";
+$password = "3156dcd4";
+$dbname = "alexiamessages";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT idcommunications FROM communications";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row["idcommunications"]. "&#13;&#10;";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+</textarea>
+
+<form action="Chat.php" method="post">
+<textarea type="text" name="text" wrap="soft" rows="10" cols="100" >
+</textarea>
+<br>
+Nom:<input type="text" name="name" size="15"><br>
+Password:<input type="text" name="password"><br>
+<input type="submit">
+</center>
+</form>
+</body>
+</html>
